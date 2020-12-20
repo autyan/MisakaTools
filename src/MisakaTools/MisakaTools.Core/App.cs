46dcbff4aa68b5ca8b0cc6c14add0fd78f65cfbd
Alex <autyan@outlook.com>
2020-12-20 16:05:13 +0800

    init mvvmcross component

---
+++ src/MisakaTools/MisakaTools.Core/App.cs
@@ -0,0 +1,14 @@
+﻿using MisakaTools.Core.ViewModel;
+using MvvmCross.ViewModels;
+
+namespace MisakaTools.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<ToolsChooseViewModel>();
+            base.Initialize();
+        }
+    }
+}
---
+++ src/MisakaTools/MisakaTools.Core/ViewModel/ToolsChooseViewModel.cs
@@ -0,0 +1,8 @@
+﻿using MvvmCross.ViewModels;
+
+namespace MisakaTools.Core.ViewModel
+{
+    public class ToolsChooseViewModel : MvxViewModel
+    {
+    }
+}
